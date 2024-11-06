714c505ae3dacacb98e18a40b1abfde458d71aa3
erytan <ntan1695123@gmail.com>
2024-11-06 19:55:46 +0700

    fix router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import {Home,QuayTra} from './page/Public'
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { Home, QuayTra } from './page/Public';
 import path from './ultils/path';
+
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path={path.PUBLIC} element={<Home />}>
-          <Route path={path.QUAYTRA} element={<QuayTra/>}/>
-        </Route>  
-      </Routes>
+      {/* Sử dụng HashRouter bao bọc Routes */}
+      <HashRouter>
+        <Routes>
+          {/* Định nghĩa route chính */}
+          <Route path={path.PUBLIC} element={<Home />} />
+          {/* Định nghĩa route con */}
+          <Route path={path.QUAYTRA} element={<QuayTra />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
