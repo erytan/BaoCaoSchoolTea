c005f08befe40de7cf7daf562ae562df3360a8ea
erytan <ntan1695123@gmail.com>
2024-11-06 20:01:22 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home, QuayTra } from './page/Public';
 import path from './ultils/path';
 
 function App() {
   return (
     <div className="App">
-      {/* Sử dụng HashRouter bao bọc Routes */}
-      <HashRouter>
+      <Router>
         <Routes>
-          {/* Định nghĩa route chính */}
           <Route path={path.PUBLIC} element={<Home />} />
-          {/* Định nghĩa route con */}
           <Route path={path.QUAYTRA} element={<QuayTra />} />
         </Routes>
-      </HashRouter>
+      </Router>
     </div>
   );
 }
